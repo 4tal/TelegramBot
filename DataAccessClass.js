1e5880c03efd74277123d2bcb56f89e1f75ca26b
4tal <idan.4tal@gmail.com>
2018-07-22 00:50:17 +0300

    DAC.js\Add future functions (CurrentState,NextOptionsForState) + Naming convensions + Add user to the DB function.

--- DataAccessClass.js
+++ DataAccessClass.js
@@ -8,30 +8,69 @@ const Cryptr = require('cryptr');
 var mongoDB = configurations.mongoDBConnectionString;
 
 
+//Users:
+
 //Not sure if it is userID/ChatId
 //Try to use the options for optional parameters.
 function addUserToDB(userName,password,userID){
     cryptr = new Cryptr(configurations.secretForEncryption);
     const encryptedPassword = cryptr.encrypt(password);
 
+    //Make sure it fits to the model.
+    Task.create({
+        UserName: userName,
+        Password: encryptedPassword,
+        userId:userID
+    },
+    function(err,herrr){
+        if(err){
+            return res.send("Error");
+        }
+        else{
+            return res.send("OK");
+        }
+    });
     
 
+}
+
+function CheckIfUserExist(){
 
 }
 
-function updateUser(){
+function UpdateUser(){
 
 }
 
-function deleteUser(){
+function DeleteUser(){
 
 }
 
 //Get the id of the state of the conversion with the bot.
-function getUserState(){
+
+
+//#####################################################################
+
+//States:
+
+function GetUserCurrentState(userID){
 
 }
 
-module.exports.addUserToDB = addUserToDB();
-module.exports.updateUser = updateUser();
-module.exports.deleteUser = deleteUser();
+function GetNextOptions(state){
+
+}
+
+//#####################################################################
+
+
+
+//Make it in one call
+module.exports.addUserToDB = AddUserToDB();
+module.exports.updateUser = UpdateUser();
+module.exports.deleteUser = DeleteUser();
+module.exports.GetUserCurrentState = GetUserCurrentState();
+module.exports.GetNextOptions = GetNextOptions();
+module.exports.CheckIfUserExist = CheckIfUserExist();
+
+
